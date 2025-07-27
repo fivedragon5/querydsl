03badab6a41d8fca64ad26346ee40f73b4195325
fad <ojy9406@naver.com>
2025-07-27 14:35:38 +0900

    Querydsl 결과조회, 검색조건, 페이징 테스트

--- src/test/java/fivedragons/querydsl/QuerydslBasicTest.java
+++ src/test/java/fivedragons/querydsl/QuerydslBasicTest.java
@@ -1,15 +1,20 @@
 package fivedragons.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import fivedragons.querydsl.entity.Member;
+import fivedragons.querydsl.entity.QMember;
 import fivedragons.querydsl.entity.Team;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static fivedragons.querydsl.entity.QMember.*;
 
@@ -83,5 +88,85 @@ public class QuerydslBasicTest {
                 .fetchOne();
 
         Assertions.assertEquals(findMember.getUsername(), "member1");
+    }
+
+    @Test
+    void resultFetchBeforeVersion() {
+        List<Member> fetch = queryFactory
+                .select(member)
+                .fetch();
+
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        results.getTotal();
+        List<Member> content = results.getResults();
+
+        long total = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+    }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순
+     * 2. 회원 이름 오름차순
+     *  - 단 2에서 회원 이름이 없을 경우, 마지막에 출력
+     */
+    @Test
+    void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = fetch.get(0);
+        Member member6 = fetch.get(1);
+        Member memberNull = fetch.get(2);
+
+        Assertions.assertEquals(member5.getUsername(), "member5");
+        Assertions.assertEquals(member6.getUsername(), "member6");
+        Assertions.assertNull(memberNull.getUsername());
+    }
+
+    @Test
+    void paging1() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        Assertions.assertEquals(result.size(), 2);
+    }
+
+    @Test
+    void paging2() {
+        QueryResults<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        Assertions.assertEquals(result.getTotal(), 4L);
+        Assertions.assertEquals(result.getResults().size(), 2);
+        Assertions.assertEquals(result.getOffset(), 1);
+        Assertions.assertEquals(result.getLimit(), 2);
     }
 }
