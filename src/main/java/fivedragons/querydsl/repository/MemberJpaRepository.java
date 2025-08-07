de11a420fc623affde0514d774d49691cd54e2f5
fad <ojy9406@naver.com>
2025-08-07 21:32:11 +0900

    Querydsl - 순수 JPA Repository 와 Querydsl

---
+++ src/main/java/fivedragons/querydsl/repository/MemberJpaRepository.java
@@ -0,0 +1,57 @@
+package fivedragons.querydsl.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import fivedragons.querydsl.entity.Member;
+import fivedragons.querydsl.entity.QMember;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static fivedragons.querydsl.entity.QMember.member;
+
+@Repository
+public class MemberJpaRepository {
+
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    public MemberJpaRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public void save(Member member)  {
+        em.persist(member);
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member findedMember = em.find(Member.class, id);
+        return Optional.ofNullable(findedMember);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery(("select m from Member m"), Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findAll_Querydsl() {
+        return queryFactory
+                .selectFrom(member)
+                .fetch();
+    }
+
+    public List<Member> findByUsername(String username) {
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", username)
+                .getResultList();
+    }
+
+    public List<Member> findByUsername_Querydsl(String username) {
+        return queryFactory
+                .selectFrom(member)
+                .where(member.username.eq(username))
+                .fetch();
+    }
+}
---
+++ src/test/java/fivedragons/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,48 @@
+package fivedragons.querydsl.repository;
+
+import fivedragons.querydsl.entity.Member;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired EntityManager em;
+    @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Test
+    void basicTest() {
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        List<Member> result1 = memberJpaRepository.findAll();
+        List<Member> result2 = memberJpaRepository.findByUsername("member1");
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+        Assertions.assertThat(result1).containsExactly(member);
+        Assertions.assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    void basicQuerydslTest() {
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        List<Member> result1 = memberJpaRepository.findAll_Querydsl();
+        List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+        Assertions.assertThat(result1).containsExactly(member);
+        Assertions.assertThat(result2).containsExactly(member);
+    }
+}
