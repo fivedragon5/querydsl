6e57a11baa7b1f36168631ac38b29e80d2d628e6
fad <ojy9406@naver.com>
2025-08-07 22:02:56 +0900

    동적 쿼리와 성능 최적화 조회 - Where 사용

--- src/test/java/fivedragons/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/fivedragons/querydsl/repository/MemberJpaRepositoryTest.java
@@ -76,4 +76,31 @@ class MemberJpaRepositoryTest {
 
         Assertions.assertThat(result).extracting("username").containsExactly("member3", "member4");
     }
+
+    @Test
+    void search() {
+        Team teamA = new Team("Team A");
+        Team teamB = new Team("Team B");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("Team B");
+
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
+
+        Assertions.assertThat(result).extracting("username").containsExactly("member4");
+    }
 }
