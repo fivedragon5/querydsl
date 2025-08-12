b989229e76b7aaf306316e4b636ba87152d34ed7
fad <ojy9406@naver.com>
2025-08-12 15:09:15 +0900

    인터페이스 지원 - QuerydslPredicateExecutor

--- src/test/java/fivedragons/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/fivedragons/querydsl/repository/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package fivedragons.querydsl.repository;
 import fivedragons.querydsl.dto.MemberSearchCondition;
 import fivedragons.querydsl.dto.MemberTeamDto;
 import fivedragons.querydsl.entity.Member;
+import fivedragons.querydsl.entity.QMember;
 import fivedragons.querydsl.entity.Team;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
@@ -11,12 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 class MemberRepositoryTest {
 
     @Autowired
@@ -118,5 +121,33 @@ class MemberRepositoryTest {
         Assertions.assertThat(result.getContent())
                 .extracting("username")
                 .containsExactly("member1", "member2", "member3");
+    }
+
+    @Test
+    void querydslPredicateExecutorTest() {
+        Team teamA = new Team("Team A");
+        Team teamB = new Team("Team B");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(
+                member.age.between(10, 40)
+                        .and(member.username.eq("member1"))
+        );
+
+        for (Member m : result) {
+            System.out.println("member = " + m);
+        }
     }
 }
